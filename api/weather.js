8dcce2700736cd3d89aca4177940c81018073274
Amor Zephyr <amorzephyr@gmail.com>
2026-01-16 18:36:33 +0500

    Create: fetch_weather endpoint that provide weather data of queried location

--- api/weather.js
+++ api/weather.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const API_ENDPOINT = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const API_KEY = "3WLSG8VHF6EQ2LXDE6EPYTL7E";
+let location = "Multan";
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/fetch_weather', async (req, res) => {
+  location = req.query.location;
+  const API_URL = `${API_ENDPOINT}${location}?key=${API_KEY}`;
+
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error(`API returned Status: ${res.status}`);
+    }
+    const data = await response.json();
+    res.json({
+      success: true,
+      message: `Data for ${location} fetched successfully`,
+      data: data
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: "Error Fetching Data",
+      error: error.message
+    })
+  }
 });
 
 app.listen(PORT, () => {
