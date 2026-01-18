1694826765b0fda3d6668c470a740373053a37a6
Amor Zephyr <amorzephyr@gmail.com>
2026-01-18 18:18:07 +0500

    Create anhour component creator functions

--- public/js/components.js
+++ public/js/components.js
@@ -1,3 +1,5 @@
+import { MiToKm, toCelsius, units } from "./utils.js";
+
 function createContainer (id, classNames, ...children) {
   const container = document.createElement('div');
   container.id = id;
@@ -22,6 +24,33 @@ function createLabeledElement(id, label, value) {
   return container;
 }
 function createAnHour (hour) {
-  
+  const time = hour.datetime.slice(0, 2);
+  let temp = hour.temp;
+  if (units.temp == '°C') {
+    temp = toCelsius(temp);
+  }
+  temp = `${temp}${units.temp}`;
+  const cloudCover = `${hour.cloudcover}%`;
+  let windspeed = hour.windspeed;
+  const conditions = hour.conditions;
+  if (units.temp == 'Km/h') {
+    windspeed = MiToKm(windspeed);
+  }
+  const timeElem = createElement(time, 'bold large value');
+  const tempElem = createElement(temp, 'bold value' );
+  const cloudCoverElem = createElement(cloudCover, 'value');
+  const conditionsElem = createElement(conditions, 'value');
+  const windspeedElem = createElement(windspeed, 'value');
+
+  const hourContainer  = createContainer( 
+    `${time}-hour`, 
+    'flex-column gap1rem card', 
+    timeElem, 
+    tempElem, 
+    cloudCoverElem, 
+    windspeedElem, 
+    conditionsElem
+  );
+  return hourContainer;
 }
-export {createContainer, createElement, createLabeledElement};
+export {createContainer, createElement, createLabeledElement, createAnHour};
